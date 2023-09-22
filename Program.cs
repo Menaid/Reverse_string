b4a909d64ee49a58eb0928cbe0d7f235d447dfc7
Menaid <menaidnocic@live.se>
2023-09-22 13:22:55 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Hej, vänligen ange ett ord ");
+
+string word = Console.ReadLine().ToLower();
+
+
+for (int i = (word.Length) -1; i >= 0; i--)
+{
+    Console.Write(word[i]);
+}
+
